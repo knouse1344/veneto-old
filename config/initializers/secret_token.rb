0be09e4115ba1ccf9f45d3ec016fe71827d6b268
Bryan <bknouse@u.rochester.edu>
2013-07-27 08:16:33 -0400

    sadfasdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venetos::Application.config.secret_token = 'd74b86d62c7b8e1492ba9051a602c9f4067f5a6f082395addd1f241d49699b82b3807ccf16622a5de40edebbd6d7ae67557f517f78afec10b6497e2fc9c774a9'
