07c87baed4cd55c2b0bc88c1eee44caaa3a0df46
Bryan <bknouse@u.rochester.edu>
2013-07-27 08:29:06 -0400

    deleted and updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,14 @@ class WelcomeController < ApplicationController
   end
 
   def lunch
+    @perin = Section.find(1)
+    @itemp = @perin.items.sort!
+    @insal = Section.find(2)
+    @itemi = @insal.items.sort!
+    @pizza = Section.find(3)
+    @item_pizza = @pizza.items.sort!
+    @pasta = Section.find(4)
+    @item_pasta = @pasta.items.sort!
   end
 
   def dinner
