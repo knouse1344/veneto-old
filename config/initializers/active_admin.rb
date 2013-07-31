a9e87a7e349393d987bc92d4f9c96632ff11d64f
Bryan <bknouse@u.rochester.edu>
2013-07-31 09:12:07 -0400

    activeadmin updates, deletes, fixed seeds

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,8 +5,8 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Venetos"
-
+  #config.site_title = "Venetos"
+  config.site_title_image = "/assets/header_logo.png"
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
