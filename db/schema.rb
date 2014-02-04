bd078e29a5b9a45dc29ef3c6c1de7d0ae01bd509
Bryan <bknouse@u.rochester.edu>
2014-02-03 21:12:14 -0500

    deleted

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204010636) do
+ActiveRecord::Schema.define(version: 20140204014926) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20140204010636) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
